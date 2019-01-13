6ea7cecdcf9f7a318105de8ba16f31cf57fd1823
John Kelleher <johnkelleher@gmail.com>
2019-01-13 14:43:17 +1100

    Updates the script file

--- run-validate-test.sh
+++ run-validate-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 
-./validate-tests "npm test" "/Users/john/Code/elliot-test/" "src/simple-function.js" "" "" ".test.js"
+./validate-tests "npm test" "./" "src/simple-function.js" "" "" ".test.js"
